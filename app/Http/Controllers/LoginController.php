2274b394e94453ed02d09c510b0c2ed50d7fc7b2
fikrideveloper <muhammad.fikri.azzumardi@gmail.com>
2023-03-03 17:01:57 +0700

    memberikan alert jika gagal login, berhasil login, berhasil logout. menampilkan halaman register. membuat fungsi register akun dengan validate hash password dan alert berhasil register

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,8 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use Illuminate\Http\Request;
 use Auth;
+use Illuminate\Support\Facades\Hash;
+use RealRashid\SweetAlert\Facades\Alert;
+
 class LoginController extends Controller
 {
     //
@@ -18,13 +22,39 @@ class LoginController extends Controller
             return redirect('dashboard');
         
         }
-        return redirect('login')->with(['errors', 'Anda gagal login']);
+        Alert::error('Username / Password Salah !!',);
+        return redirect('login');
         
 
     }
 
     public function logout(Request $request){
         Auth::logout();
+
+        Alert::success('Berhasil LOGOUT !!');
         return redirect('/login');
+    }
+
+    public function register(){
+        return view('account.register');
+    }
+
+    public function create(Request $request){
+        $request->validate([
+            'nama' => 'required',
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $create = [
+            'name' =>$request->nama,
+            'email' =>$request->email,
+            'password' => Hash::make($request->password)
+        ];
+
+        User::create($create);
+
+        return redirect('/login')->with('success', 'Berhasil membuat Akun');
+        // return redirect('/preview_rapat')->with('success', 'Data berhasil ditambahkan !!');
     }
 }
