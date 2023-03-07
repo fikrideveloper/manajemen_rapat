fcbf47fea11a543204dae122559f234652f54a58
fikrideveloper <muhammad.fikri.azzumardi@gmail.com>
2023-03-07 22:38:42 +0700

    update footer main layout

--- resources/views/layouts/main_new.blade.php
+++ resources/views/layouts/main_new.blade.php
@@ -1157,11 +1157,10 @@
     </section> --}}
 
   </main><!-- End #main -->
-
   <!-- ======= Footer ======= -->
   <footer id="footer" class="footer">
     <div class="copyright">
-      &copy; Copyright <strong><span>Manajemen Rapat</span></strong>. All Rights Reserved
+      &copy; Copyright <strong><a href="">Manajemen Rapat</a></strong>. All Rights Reserved
     </div>
     {{-- <div class="credits">
       <!-- All the links in the footer should remain intact. -->
