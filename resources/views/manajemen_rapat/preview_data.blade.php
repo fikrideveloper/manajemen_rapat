1704f53c3dac8894e839cddc7d75bc41ccf03a31
fikrideveloper <muhammad.fikri.azzumardi@gmail.com>
2023-03-23 07:49:08 +0700

    fitur search data

--- resources/views/manajemen_rapat/preview_data.blade.php
+++ resources/views/manajemen_rapat/preview_data.blade.php
@@ -6,6 +6,13 @@
 
 @section('menu_data_rapat','active')
 
+@section('fitur_search')
+    <form class="search-form d-flex align-items-center" method="get" action="/preview_rapat/search">
+      <input type="text" value="{{ request('search') }}" name="search" placeholder="Search" title="Enter search keyword">
+      <button type="submit" title="Search"><i class="bi bi-search"></i></button>
+    </form>
+@endsection
+
 @section('menu')
     <li class="breadcrumb-item">Manajemen Rapat</li>
     <li class="breadcrumb-item active">Preview Data Rapat</li>
@@ -25,6 +32,7 @@
                   <thead>
                     <tr>
                       {{-- <th scope="col"></th> --}}
+                      <th scope="col">No.</th>
                       <th scope="col">Nama Rapat</th>
                       <th scope="col">Tanggal</th>
                       <th scope="col">Waktu</th>
@@ -34,8 +42,11 @@
                     </tr>
                   </thead>
                   <tbody>
+                    <?php $no= 1; ?>
+
                     @foreach ($data_rapat as $data)
                     <tr>
+                      <td>{{ $no }}</td>
                       <td>{{ $data->nama_rapat }}</td>
 
                       {{-- Date disini untuk mengatur format tanggal dari urut hari ke tahun --}}
@@ -45,8 +56,9 @@
                       <td>{{ date('H:i', strtotime($data->waktu_rapat)) }} WIB</td>
                       <td>{{ $data->kategori }}</td>
                       <td><a href="/view_rapat/{{ $data->id }}">{{ Str::limit($data->hasil_rapat,15, '...') }}</a></td>
-                      <td><a class="btn btn-warning" href="#"><i class=" bi bi-pencil-square"></i></a> <a class="btn btn-danger" id="#delete" onclick="return confirm('Yakin ingin MENGHAPUS?');"  href="/hapus_data_rapat/{{ $data->id }}"><i class="bi bi-trash-fill"></i></a></td>
+                      <td><a class="btn btn-warning" href="/edit_rapat/{{ $data->id }}"><i class=" bi bi-pencil-square"></i></a> <a class="btn btn-danger" id="#delete" onclick="return confirm('Yakin ingin MENGHAPUS?');"  href="/hapus_data_rapat/{{ $data->id }}"><i class="bi bi-trash-fill"></i></a></td>
                     </tr>
+                    <?php $no++; ?>
                     @endforeach
                     
                     
@@ -55,240 +67,12 @@
                 </table>
                 
                   {{ $data_rapat->links() }}
-                
+                  <span class="text-sm">Hasil data {{ $total_rapat }}</span>
                 
                 <!-- End Table with stripped rows -->
     
               </div>
             </div>
-    
-            {{-- <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Table with hoverable rows</h5>
-    
-                <!-- Table with hoverable rows -->
-                <table class="table table-hover">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Name</th>
-                      <th scope="col">Position</th>
-                      <th scope="col">Age</th>
-                      <th scope="col">Start Date</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Brandon Jacob</td>
-                      <td>Designer</td>
-                      <td>28</td>
-                      <td>2016-05-25</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">2</th>
-                      <td>Bridie Kessler</td>
-                      <td>Developer</td>
-                      <td>35</td>
-                      <td>2014-12-05</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">3</th>
-                      <td>Ashleigh Langosh</td>
-                      <td>Finance</td>
-                      <td>45</td>
-                      <td>2011-08-12</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">4</th>
-                      <td>Angus Grady</td>
-                      <td>HR</td>
-                      <td>34</td>
-                      <td>2012-06-11</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">5</th>
-                      <td>Raheem Lehner</td>
-                      <td>Dynamic Division Officer</td>
-                      <td>47</td>
-                      <td>2011-04-19</td>
-                    </tr>
-                  </tbody>
-                </table>
-                <!-- End Table with hoverable rows -->
-    
-              </div>
-            </div> --}}
-    
-            {{-- <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Bordered Table</h5>
-                <p>Add <code>.table-bordered</code> for borders on all sides of the table and cells.</p>
-                <!-- Bordered Table -->
-                <table class="table table-bordered">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Name</th>
-                      <th scope="col">Position</th>
-                      <th scope="col">Age</th>
-                      <th scope="col">Start Date</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Brandon Jacob</td>
-                      <td>Designer</td>
-                      <td>28</td>
-                      <td>2016-05-25</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">2</th>
-                      <td>Bridie Kessler</td>
-                      <td>Developer</td>
-                      <td>35</td>
-                      <td>2014-12-05</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">3</th>
-                      <td>Ashleigh Langosh</td>
-                      <td>Finance</td>
-                      <td>45</td>
-                      <td>2011-08-12</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">4</th>
-                      <td>Angus Grady</td>
-                      <td>HR</td>
-                      <td>34</td>
-                      <td>2012-06-11</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">5</th>
-                      <td>Raheem Lehner</td>
-                      <td>Dynamic Division Officer</td>
-                      <td>47</td>
-                      <td>2011-04-19</td>
-                    </tr>
-                  </tbody>
-                </table>
-                <!-- End Bordered Table -->
-    
-                <p><a href="https://getbootstrap.com/docs/5.0/utilities/borders/#border-color" target="_blank">Border color utilities</a> can be added to change colors:</p>
-    
-                <!-- Primary Color Bordered Table -->
-                <table class="table table-bordered border-primary">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Name</th>
-                      <th scope="col">Position</th>
-                      <th scope="col">Age</th>
-                      <th scope="col">Start Date</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Brandon Jacob</td>
-                      <td>Designer</td>
-                      <td>28</td>
-                      <td>2016-05-25</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">2</th>
-                      <td>Bridie Kessler</td>
-                      <td>Developer</td>
-                      <td>35</td>
-                      <td>2014-12-05</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">3</th>
-                      <td>Ashleigh Langosh</td>
-                      <td>Finance</td>
-                      <td>45</td>
-                      <td>2011-08-12</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">4</th>
-                      <td>Angus Grady</td>
-                      <td>HR</td>
-                      <td>34</td>
-                      <td>2012-06-11</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">5</th>
-                      <td>Raheem Lehner</td>
-                      <td>Dynamic Division Officer</td>
-                      <td>47</td>
-                      <td>2011-04-19</td>
-                    </tr>
-                  </tbody>
-                </table>
-                <!-- End Primary Color Bordered Table -->
-    
-              </div>
-            </div> --}}
-    
-            {{-- <div class="card">
-              <div class="card-body">
-                <h5 class="card-title">Small tables</h5>
-                <p>Add <code>.table-sm</code> to make any <code>.table</code> more compact by cutting all cell padding in half.</p>
-                <!-- Small tables -->
-                <table class="table table-sm">
-                  <thead>
-                    <tr>
-                      <th scope="col">#</th>
-                      <th scope="col">Name</th>
-                      <th scope="col">Position</th>
-                      <th scope="col">Age</th>
-                      <th scope="col">Start Date</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Brandon Jacob</td>
-                      <td>Designer</td>
-                      <td>28</td>
-                      <td>2016-05-25</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">2</th>
-                      <td>Bridie Kessler</td>
-                      <td>Developer</td>
-                      <td>35</td>
-                      <td>2014-12-05</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">3</th>
-                      <td>Ashleigh Langosh</td>
-                      <td>Finance</td>
-                      <td>45</td>
-                      <td>2011-08-12</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">4</th>
-                      <td>Angus Grady</td>
-                      <td>HR</td>
-                      <td>34</td>
-                      <td>2012-06-11</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">5</th>
-                      <td>Raheem Lehner</td>
-                      <td>Dynamic Division Officer</td>
-                      <td>47</td>
-                      <td>2011-04-19</td>
-                    </tr>
-                  </tbody>
-                </table>
-                <!-- End small tables -->
-    
-              </div>
-            </div> --}}
-    
           </div>
       
     </div>
