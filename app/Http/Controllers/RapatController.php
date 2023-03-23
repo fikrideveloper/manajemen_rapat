9812ad2e3c1608a507ab8801ffcb65f391d09252
fikrideveloper <muhammad.fikri.azzumardi@gmail.com>
2023-03-23 07:24:22 +0700

    menambahkan fitur search data, edit data. tambah data Rapat

--- app/Http/Controllers/RapatController.php
+++ app/Http/Controllers/RapatController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Kategori;
 use File;
 use App\Rapat;
+use App\User;
 use Illuminate\Http\Request;
 use RealRashid\SweetAlert\Facades\Alert;
 
@@ -12,10 +14,34 @@ class  RapatController extends Controller
     //
     public function index(){
 
-        // $data_rapat = Rapat::all();
+
+        $total_rapat = Rapat::count();
         return view('manajemen_rapat.preview_data',[
-            'data_rapat' => Rapat::paginate(5)
+            'data_rapat' => Rapat::paginate(5),
+            'total_rapat' => $total_rapat
         ]);
+    }
+
+    public function search(Request $request){
+        $cari = $request->search;
+
+        // Fitur searching untuk mencari data sesuai kolom database
+        $cari_rapat= Rapat::where('nama_rapat', 'like','%'.$cari.'%')
+                            ->orWhere('kategori', 'like', '%'.$cari. '%')
+                            ->orWhere('hasil_rapat', 'like', '%'.$cari. '%')
+                            ->orWhere('tanggal_rapat', 'like', '%'.$cari. '%');
+
+        $cari_rapat = $cari_rapat->paginate(5);       
+        $cari_rapat = $cari_rapat->appends($request->all());
+        $total_rapat = $cari_rapat->count();
+        
+        // Mengirim request cari data dari user ke halaman preview data
+        return view('manajemen_rapat.preview_data',
+        ['data_rapat' => $cari_rapat], 
+        ['total_rapat' => $total_rapat]
+            
+        );
+
     }
 
     public function view($id){
@@ -27,19 +53,21 @@ class  RapatController extends Controller
         ]);
     }
 
-
-    public function tambah(){
-        return view('manajemen_rapat.tambah_rapat');
+    public function edit($id){
+        $kategori = Kategori::get()->all();
+        return view('manajemen_rapat.edit_rapat', [
+            'edit_rapat' => Rapat::where('id', $id)->first(),
+            'kategori_rapat' => $kategori
+        ]);
     }
 
-    public function create_data(Request $request){
-
+    public function update(Request $request,$id){
         $request->validate([
             'nama' => 'required',
             'tanggal' => 'required',
             'waktu' => 'required',
             'kategori' => 'required',
-            'dokumentasi' => 'file|image|nullable|mimes:jpg,png,jpeg', 
+            'dokumentasi' => 'file|image|nullable|mimes:jpg,png,jpeg|max:10240', 
             'hasil' => 'required',
             
         ],[
@@ -49,6 +77,88 @@ class  RapatController extends Controller
             'waktu.required' => 'Waktu harus diisi',
             'kategori.required' => 'Kategori harus diisi',
             'dokumentasi.image' => 'Dokumen harus JPG, PNG, atau PNG',
+            'dokumentasi.max' => 'Dokumen Maksimal 10MB',
+            // 'dokumentasi.mimes' => 'Dokumen harus JPG, PNG, atau PNG',
+            'hasil.required' => 'Hasil Rapat harus diisi',
+        ]);
+
+        if (request()->file('dokumentasi')) {
+
+            if ($request->old_image) {
+                
+                File::delete('data_gambar/'.$request->old_image);
+            }
+
+            $gambar= $request->file('dokumentasi');
+            $tanggal = date('d-m-Y');
+            $angka_acak = rand(1,999);
+
+            $ambil_nama = $gambar->getClientOriginalName();
+
+            $nama_file = $tanggal."_".$angka_acak."_".$ambil_nama;
+
+            $gambar->move('data_gambar/',$nama_file);
+
+
+        } else {
+            // jika user tidak mengganti gambar maka perbolehkan update data tanpa gambar
+            $gambar= null;
+
+            // Jika user tidak mengganti gambar maka biarkan gambar yang lama
+            $nama_file = $request->old_image;
+        }
+
+
+        $edit =[
+            'nama_rapat' => $request->nama,
+            'tanggal_rapat' => $request->tanggal,
+            'waktu_rapat' => $request->waktu,
+            'kategori' => $request->kategori,
+            'gambar' => $nama_file,
+            'hasil_rapat' => $request->hasil,
+            
+            
+        ];
+
+        // return dd($edit);
+
+        $rapat = Rapat::where('id',$id)->update($edit);
+        // dd($rapat);
+
+        Alert::success('Update Rapat Berhasil !!');
+        return redirect('/preview_rapat');
+    }
+
+
+    public function tambah(){
+        // Mengirim data kategori ke inputan tambah rapat
+        $kategori = Kategori::get()->all();
+
+        return view('manajemen_rapat.tambah_rapat',[
+            'kategori_rapat' => $kategori
+
+        ]);
+    }
+
+    public function create_data(Request $request){
+
+        $request->validate([
+            'nama' => 'required',
+            'tanggal' => 'required',
+            'waktu' => 'required',
+            'kategori' => 'required',
+            'dokumentasi' => 'file|image|nullable|mimes:jpg,png,jpeg|max:10240', 
+            'hasil' => 'required',
+            
+        ],[
+            
+            'nama.required' => 'Nama harus diisi',
+            'tanggal.required' => 'Tanggal harus diisi',
+            // 'tanggal.unique'=> 'Tanggal rapat sudah ada',
+            'waktu.required' => 'Waktu harus diisi',
+            'kategori.required' => 'Kategori harus diisi',
+            'dokumentasi.image' => 'Dokumen harus JPG, PNG, atau PNG',
+            'dokumentasi.max' => 'Dokumen Maksimal 10MB',
             // 'dokumentasi.mimes' => 'Dokumen harus JPG, PNG, atau PNG',
             'hasil.required' => 'Hasil Rapat harus diisi',
         ]);
